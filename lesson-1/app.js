2d8c9f122eafd38781e2a4087da5dbdb93ff7bcd
yth345 <48090907+yth345@users.noreply.github.com>
2021-01-17 20:58:45 +0800

    增加搜尋、修改的功能

--- lesson-1/app.js
+++ lesson-1/app.js
@@ -1,5 +1,6 @@
 const routeList = document.querySelector("#route-list");
 const routeForm = document.querySelector("#add-route-form");
+const searchRouteForm = document.querySelector('#search-route-form');
 
 function renderRoute(doc){
     let li = document.createElement('li');
@@ -7,17 +8,49 @@ function renderRoute(doc){
     let origin = document.createElement('span');
     let destination = document.createElement('span');
     let cross = document.createElement('div');
+    let revise = document.createElement('button');
 
     li.setAttribute('data-id', doc.id);
     identity.textContent = doc.data().ID;
     origin.textContent = doc.data().origin;
     destination.textContent = doc.data().destination;
     cross.textContent = 'x';
+    
+    revise.textContent = 'revise';
+    revise.setAttribute("onclick", "displayRevise()");
 
     li.appendChild(identity);
     li.appendChild(origin);
     li.appendChild(destination);
     li.appendChild(cross);
+    li.appendChild(revise);
+
+    let reviseForm = document.createElement('form');
+    reviseForm.id = "reviseRouteForm";
+    reviseForm.style.display = "none";
+
+    let inputID = document.createElement('input');
+    inputID.setAttribute("type", "text");
+    inputID.setAttribute("name", "inid");
+    inputID.setAttribute("placeholder", "route ID");
+
+    let inputOrigin = document.createElement('input');
+    inputOrigin.setAttribute("type", "text");
+    inputOrigin.setAttribute("name", "inorigin")
+    inputOrigin.setAttribute("placeholder", "origin");
+
+    let inputDestination = document.createElement('input');
+    inputDestination.setAttribute("type", "text");
+    inputDestination.setAttribute("name", "indes");
+    inputDestination.setAttribute("placeholder", "destination");
+
+    let reviseButton = document.createElement('button');
+    reviseButton.textContent = 'submit';
+
+    reviseForm.appendChild(inputID);
+    reviseForm.appendChild(inputOrigin);
+    reviseForm.appendChild(inputDestination)
+    reviseForm.appendChild(reviseButton);
 
     routeList.appendChild(li);
 
@@ -27,14 +60,30 @@ function renderRoute(doc){
         let id = e.target.parentElement.getAttribute('data-id');
         db.collection('routes').doc(id).delete();
     })
+
+    // revising data
+    reviseForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('routes').doc(id).update({
+            ID: reviseForm.inid.value,
+            origin: reviseForm.inorigin.value,
+            destination: reviseForm.indes.value
+        });
+        reviseForm.inid.value = '';
+        reviseForm.inorigin.value = '';
+        reviseForm.indes.value ='';
+    })
 }
 
-// getting data
-// db.collection('routes').orderBy('ID').get().then((snapshot) => {
-//     snapshot.docs.forEach(doc => {
-//         renderRoute(doc);
-//     })
-// });
+function displayRevise(){
+    let x = document.getElementById("reviseRouteForm");
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
 
 // saving data
 routeForm.addEventListener('submit', (e) => {
@@ -47,6 +96,49 @@ routeForm.addEventListener('submit', (e) => {
     routeForm.id.value = '';
     routeForm.origin.value = '';
     routeForm.destination.value = '';
+});
+
+let clearSearchRoute = document.createElement('button');
+clearSearchRoute.textContent = "clear";
+searchRouteForm.appendChild(clearSearchRoute);
+
+// search data
+searchRouteForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    while (routeList.firstChild){
+        routeList.removeChild(routeList.firstChild);
+    }
+    db.collection('routes').where('ID', '==', searchRouteForm.id.value).onSnapshot(snapshot => {
+        let changes = snapshot.docChanges();
+        changes.forEach(change => {
+            if (change.type == 'added'){
+                renderRoute(change.doc);
+            } else if (change.type == 'removed'){
+                let li = routeList.querySelector('[data-id=' + change.doc.id + ']');
+                routeList.removeChild(li);
+            }
+        });
+    });
+});
+
+// clear data after search
+clearSearchRoute.addEventListener("click", (e) => {
+    e.preventDefault();
+    while (routeList.firstChild){
+        routeList.removeChild(routeList.firstChild);
+    }
+    db.collection('routes').orderBy('ID').onSnapshot(snapshot => {
+        let changes = snapshot.docChanges();
+        changes.forEach(change => {
+            if (change.type == 'added'){
+                renderRoute(change.doc);
+            } else if (change.type == 'removed'){
+                let li = routeList.querySelector('[data-id=' + change.doc.id + ']');
+                routeList.removeChild(li);
+            }
+        });
+    });
+    searchRouteForm.id.value = '';
 });
 
 // real-time listener
